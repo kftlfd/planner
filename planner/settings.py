a76e957edf691ef186e4c5ade421365be90a1697
kftlfd <kftlfd@ex.ua>
2022-06-13 18:07:10 +0300

    add main template

--- planner/settings.py
+++ planner/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'planner.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend/templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
