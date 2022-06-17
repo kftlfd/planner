faa10d41cb265dd9558a12ec9486321c805cd550
kftlfd <kftlfd@ex.ua>
2022-06-17 16:57:52 +0300

    formatting

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,12 +2,12 @@
 // https://docs.djangoproject.com/en/4.0/ref/csrf/#acquiring-the-token-if-csrf-use-sessions-and-csrf-cookie-httponly-are-false
 function getCookie(name) {
   let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();
       // Does this cookie string begin with the name we want?
-      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+      if (cookie.substring(0, name.length + 1) === name + "=") {
         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
         break;
       }
@@ -16,11 +16,13 @@ function getCookie(name) {
   return cookieValue;
 }
 
-export const API = {
-  root: '/api/',
-  auth: '/auth/',
-  csrftoken: getCookie('csrftoken'),
-  register: '/auth/register',
-  login: '/auth/login',
-  logout: '/auth/logout'
-}
+const API = {
+  root: "/api/",
+  auth: "/auth/",
+  csrftoken: getCookie("csrftoken"),
+  register: "/auth/register",
+  login: "/auth/login",
+  logout: "/auth/logout",
+};
+
+export default API;
