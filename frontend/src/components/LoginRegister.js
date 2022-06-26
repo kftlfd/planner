f6fde4e5b6fb8fbc2329617a4d2b45fad430c4f6
kftlfd <kftlfd@ex.ua>
2022-06-26 20:21:05 +0300

    redo forms with material ui

--- frontend/src/components/LoginRegister.js
+++ frontend/src/components/LoginRegister.js
@@ -1,98 +1,152 @@
-import React, { useState, useId } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useAuth } from "../auth";
-import "./LoginRegister.scss";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import CheckIcon from "@mui/icons-material/Check";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
 export function Register(props) {
   const auth = useAuth();
-  const registerFormId = useId();
-  const [registerRes, setRegisterRes] = useState(null);
+  const [formResponse, setFormResponse] = useState(null);
   const redirect = useNavigate();
 
+  const loggedIn = auth.user ? true : false;
+  if (loggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  function sendRegisterForm(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    auth.register(formData).then((res) => {
+      setFormResponse(res);
+      if (res.success) {
+        setTimeout(() => {
+          redirect("/");
+        }, 2000);
+      } else {
+        console.error("sendRegisterForm", res);
+      }
+    });
+  }
+
   return (
-    <form id={registerFormId} className="AuthForm">
-      <div>
-        <input type={"text"} name={"username"} placeholder={"Username"}></input>
-        {registerRes?.username && <div>{registerRes.username}</div>}
-      </div>
-      <div>
-        <input
-          type={"password"}
-          name={"password1"}
-          placeholder={"Password"}
-        ></input>
-        {registerRes?.password1 && <div>{registerRes.password1}</div>}
-      </div>
-      <div>
-        <input
-          type={"password"}
-          name={"password2"}
-          placeholder={"Confirm Password"}
-        ></input>
-        {registerRes?.password2 && <div>{registerRes.password2}</div>}
-      </div>
-      <div>
-        <button
-          onClick={async (event) => {
-            event.preventDefault();
-            let formData = new FormData(
-              document.getElementById(registerFormId)
-            );
-            let res = await auth.register(formData);
-            setRegisterRes(res);
-            if (res.success) {
-              setTimeout(() => {
-                redirect("/");
-              }, 2000);
-            }
-          }}
-        >
-          Register
-        </button>
-      </div>
-      {registerRes?.success && <div>{registerRes.success}</div>}
-      {registerRes?.error && <div>{registerRes.error}</div>}
+    <form className="AuthForm" onSubmit={sendRegisterForm}>
+      <TextField
+        name="username"
+        type="text"
+        label="Username"
+        variant="filled"
+        size="small"
+        error={formResponse?.username ? true : false}
+        helperText={formResponse?.username}
+      />
+      <TextField
+        name="password1"
+        type="password"
+        label="Password"
+        variant="filled"
+        size="small"
+        error={formResponse?.password1 ? true : false}
+        helperText={formResponse?.password1}
+      />
+      <TextField
+        name="password2"
+        type="password"
+        label="Confirm Password"
+        variant="filled"
+        size="small"
+        error={formResponse?.password2 ? true : false}
+        helperText={formResponse?.password2}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color={formResponse?.success ? "success" : "primary"}
+        endIcon={formResponse?.success ? <CheckIcon /> : null}
+      >
+        {formResponse?.success ? "Sighned Up" : "Sign Up"}
+      </Button>
+      {formResponse?.serverError && (
+        // if can't connect to server
+        <Alert severity="warning">
+          <AlertTitle>Server Error</AlertTitle>
+          {formResponse.serverError}
+        </Alert>
+      )}
+      {formResponse?.error && (
+        // if can't create new user
+        <Alert severity="warning">
+          <AlertTitle>Server Error</AlertTitle>
+          {formResponse.error}
+        </Alert>
+      )}
     </form>
   );
 }
 
 export function Login() {
   const auth = useAuth();
-  const loginFormId = useId();
-  const [loginRes, setLoginRes] = useState(null);
+  const [formResponse, setFormResponse] = useState(null);
   const redirect = useNavigate();
 
+  const loggedIn = auth.user ? true : false;
+  if (loggedIn) {
+    return <Navigate to="/" />;
+  }
+
+  function sendLoginForm(event) {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    auth.login(formData).then((res) => {
+      setFormResponse(res);
+      if (res.success) {
+        setTimeout(() => {
+          redirect("/");
+        }, 1000);
+      } else {
+        console.error("sendLoginForm", res);
+      }
+    });
+  }
+
   return (
-    <form id={loginFormId} className="AuthForm">
-      <div>
-        <input type={"text"} name={"username"} placeholder={"Username"}></input>
-      </div>
-      <div>
-        <input
-          type={"password"}
-          name={"password"}
-          placeholder={"Password"}
-        ></input>
-      </div>
-      <div>
-        <button
-          onClick={async (event) => {
-            event.preventDefault();
-            let formData = new FormData(document.getElementById(loginFormId));
-            let res = await auth.login(formData);
-            setLoginRes(res);
-            if (res.success) {
-              setTimeout(() => {
-                redirect("/");
-              }, 1000);
-            }
-          }}
-        >
-          Log In
-        </button>
-      </div>
-      {loginRes?.success && <div>{loginRes.success}</div>}
-      {loginRes?.error && <div>{loginRes.error}</div>}
+    <form className="AuthForm" onSubmit={sendLoginForm}>
+      <TextField
+        name="username"
+        type="text"
+        label="Username"
+        variant="filled"
+        size="small"
+        autoComplete="false"
+        error={formResponse?.error ? true : false}
+      />
+      <TextField
+        name="password"
+        type="password"
+        label="Password"
+        variant="filled"
+        size="small"
+        error={formResponse?.error ? true : false}
+        helperText={formResponse?.error}
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color={formResponse?.success ? "success" : "primary"}
+        endIcon={formResponse?.success ? <CheckIcon /> : null}
+      >
+        {formResponse?.success ? "Logged In" : "Log In"}
+      </Button>
+      {formResponse?.serverError && (
+        // can't connect to server
+        <Alert severity="warning">
+          <AlertTitle>Server Error</AlertTitle>
+          {formResponse.serverError}
+        </Alert>
+      )}
     </form>
   );
 }
