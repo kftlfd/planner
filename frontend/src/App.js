c7b75159e60bbbe5ab04e9a25595c761f6f52f1d
kftlfd <kftlfd@ex.ua>
2022-06-27 15:20:54 +0300

    optimize routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,35 +8,29 @@ import { Register, Login } from "./components/LoginRegister";
 
 export default function App(props) {
   const auth = useAuth();
-  const notLoggedIn = auth.user ? false : true;
+  const loggedIn = auth.user ? true : false;
 
   if (auth.loading) {
     return <div>Loading app</div>;
-  }
-
-  if (notLoggedIn) {
-    return (
-      <>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Navigate to="/welcome" />} />
-          <Route path="/welcome" element={<Welcome />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/*" element={<div>404 not found</div>} />
-        </Routes>
-      </>
-    );
   }
 
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={loggedIn ? <Home /> : <Navigate to="/welcome" />}
+        />
         <Route path="/welcome" element={<Welcome />} />
-        <Route path="/register" element={<Navigate to="/" />} />
-        <Route path="/login" element={<Navigate to="/" />} />
+        <Route
+          path="/register"
+          element={loggedIn ? <Navigate to="/" /> : <Register />}
+        />
+        <Route
+          path="/login"
+          element={loggedIn ? <Navigate to="/" /> : <Login />}
+        />
         <Route path="/*" element={<div>404 not found</div>} />
       </Routes>
     </>
