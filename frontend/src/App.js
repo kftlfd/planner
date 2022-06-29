d1110755a43a53db924138c9b3664b7deb185e49
kftlfd <kftlfd@ex.ua>
2022-06-29 22:48:43 +0300

    move Projects Context Provider to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
+import ProvideProjects from "./ProjectsContext";
 import Main from "./components/Main";
+import Project from "./components/Project";
+import ProjectTasks from "./components/ProjectTasks";
 import Welcome from "./components/Welcome";
 import { Register, Login } from "./components/LoginRegister";
 import NotFoundError from "./components/NotFoundError";
-import Project from "./components/Project";
-import ProjectTasks from "./components/ProjectTasks";
 
-export default function App(props) {
+export default function App() {
   const auth = useAuth();
-  const loggedIn = auth.user ? true : false;
 
   if (auth.loading) {
     return <div>Loading app</div>;
   }
 
   return (
-    <>
+    <ProvideProjects>
       <Routes>
         <Route path="/" element={<Main />}>
           <Route path="project" element={<Project />}>
@@ -29,6 +29,6 @@ export default function App(props) {
         <Route path="/login" element={<Login />} />
         <Route path="/*" element={<NotFoundError />} />
       </Routes>
-    </>
+    </ProvideProjects>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,16 +4,13 @@ import App from "./App";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import ProvideAuth from "./AuthContext";
-import ProvideProjects from "./ProjectsContext";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <ProvideAuth>
-        <ProvideProjects>
-          <App />
-        </ProvideProjects>
+        <App />
       </ProvideAuth>
     </BrowserRouter>
   </React.StrictMode>
