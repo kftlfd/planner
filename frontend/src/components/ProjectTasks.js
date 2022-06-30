67c15475a3a9428ce445a7ed52e7fe624b207a5b
kftlfd <kftlfd@ex.ua>
2022-06-30 16:12:56 +0300

    add taskAddForm, list tasks via Task component

--- frontend/src/components/ProjectTasks.js
+++ frontend/src/components/ProjectTasks.js
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useProjects } from "../ProjectsContext";
+import Task from "./Task";
 
 export default function ProjectTasks(props) {
-  const { projectId } = useParams();
-  const { getProjectTasks } = useProjects();
+  const { getProjectTasks, handleTasks } = useProjects();
+  const params = useParams();
   const [tasks, setTasks] = useState([]);
+  const [projectId, setProjectId] = useState(Number(params.projectId));
+
+  const [addTaskTitle, setAddTaskTitle] = useState("");
+  const handleAddTaskTitleChange = (e) => setAddTaskTitle(e.target.value);
 
   async function loadTasks(projectId) {
     setTasks([]);
-    setTasks(await getProjectTasks(Number(projectId)));
+    setTasks(await getProjectTasks(projectId));
   }
 
   useEffect(() => {
@@ -20,6 +25,38 @@ export default function ProjectTasks(props) {
     loadTasks(projectId);
   }, [projectId]);
 
+  useEffect(() => {
+    setProjectId(Number(params.projectId));
+  }, [params.projectId]);
+
+  const taskAddForm = (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        handleTasks.create(projectId, addTaskTitle);
+      }}
+    >
+      <input
+        type={"text"}
+        placeholder={"New task"}
+        value={addTaskTitle}
+        onChange={handleAddTaskTitleChange}
+      />
+      <button type={"submit"} disabled={!addTaskTitle}>
+        + Add task
+      </button>
+    </form>
+  );
+
+  const tasksList = tasks.map((item, index) => {
+    if (item.id) {
+      return <Task key={`pj-${projectId}-task-${item.id}`} task={item} />;
+    }
+    if (item.error) {
+      return <div key={`pj-${projectId}-err-${index}`}>{item.error}</div>;
+    }
+  });
+
   return (
     <div
       style={{
@@ -29,19 +66,8 @@ export default function ProjectTasks(props) {
       }}
     >
       <h4>{"<ProjectTasks.js>"}</h4>
-      <div>Project {projectId}</div>
-      <div>
-        {tasks.map((item, index) => {
-          if (item.id) {
-            return (
-              <div key={`pj-${projectId}-task-${item.id}`}>{item.title}</div>
-            );
-          }
-          if (item.error) {
-            return <div key={`pj-${projectId}-err-${index}`}>{item.error}</div>;
-          }
-        })}
-      </div>
+      {taskAddForm}
+      {tasksList}
     </div>
   );
 }
