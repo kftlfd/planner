0cfaa02a79bb6f7d0369eba48fd725d20ba722f5
kftlfd <kftlfd@ex.ua>
2022-07-02 19:23:04 +0300

    improve header, add options menu

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -1,10 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams, Outlet } from "react-router-dom";
 import { useProjects } from "../ProjectsContext";
+
+import Typography from "@mui/material/Typography";
+import {
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Divider,
+  Dialog,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 export default function Project(props) {
   const { projectId } = useParams();
   const { projects } = useProjects();
+
+  const Header = (props) => (
+    <Typography
+      variant="h4"
+      component="div"
+      gutterBottom
+      sx={{ display: "flex", width: "100%", justifyContent: "space-between" }}
+    >
+      {props.children}
+    </Typography>
+  );
+
+  const ProjectOptionsMenu = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleOpen = (e) => setAnchorEl(e.currentTarget);
+    const handleClose = () => setAnchorEl(null);
+
+    return (
+      <>
+        <IconButton
+          id="project-options-button"
+          aria-controls={open ? "project-options-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleOpen}
+        >
+          <MoreVertIcon />
+        </IconButton>
+        <Menu
+          id="project-options-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>Rename</MenuItem>
+          <Divider />
+          <MenuItem onClick={handleClose}>Delete</MenuItem>
+        </Menu>
+      </>
+    );
+  };
+
+  const ProjectRenameModal = () => {
+    return (
+      <Dialog open={false} onClose={() => {}}>
+        <DialogTitle>Rename project</DialogTitle>
+        <form>
+          <TextField />
+          <Button />
+        </form>
+      </Dialog>
+    );
+  };
+
+  const ProjectDeleteModal = () => {
+    return (
+      <Dialog open={false} onClose={() => {}}>
+        <DialogTitle>Delete project [project name] ?</DialogTitle>
+        <Button>Delete</Button>
+        <Button>Cancel</Button>
+      </Dialog>
+    );
+  };
 
   return (
     <div
@@ -15,12 +95,19 @@ export default function Project(props) {
     >
       {projectId ? (
         projects[Number(projectId)] ? (
-          <h4>{projects[Number(projectId)].name}</h4>
+          <>
+            <Header>
+              {projects[Number(projectId)].name}
+              <ProjectOptionsMenu />
+            </Header>
+            <ProjectRenameModal />
+            <ProjectDeleteModal />
+          </>
         ) : (
-          <h4>Project {projectId} not found</h4>
+          <Header>Project not found</Header>
         )
       ) : (
-        <h4>Select a project</h4>
+        <Header>Select a project</Header>
       )}
       <Outlet />
     </div>
