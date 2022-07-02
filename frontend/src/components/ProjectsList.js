a9fca91e8e1273a1874e3ec22097c5e251977173
kftlfd <kftlfd@ex.ua>
2022-07-02 21:17:14 +0300

    remove ProjectEdit modal, refactor ProjectAdd modal; new ProjectCreate button

--- frontend/src/components/ProjectsList.js
+++ frontend/src/components/ProjectsList.js
@@ -1,150 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useProjects } from "../ProjectsContext";
 
-import Divider from "@mui/material/Divider";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  IconButton,
+} from "@mui/material";
 import ListIcon from "@mui/icons-material/List";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-
-function ProjectCreateDialog(props) {
-  const { open, onClose, handleProjects } = props;
-
-  const [nameValue, setNameValue] = useState("");
-  const handleNameChange = (e) => setNameValue(e.target.value);
-
-  const handleClose = () => onClose();
-
-  const handleCreate = async (name) => {
-    await handleProjects.create(name);
-    onClose();
-  };
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <div
-        style={{
-          padding: "1rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-        }}
-      >
-        <DialogTitle>Add new project</DialogTitle>
-
-        <form
-          name="create"
-          onSubmit={(event) => {
-            event.preventDefault();
-            handleCreate(event.target.name.value);
-          }}
-        >
-          <TextField
-            type="text"
-            name="name"
-            label="Name"
-            value={nameValue}
-            onChange={handleNameChange}
-          />
-          <Button type="submit" disabled={!nameValue}>
-            Create
-          </Button>
-        </form>
-      </div>
-    </Dialog>
-  );
-}
-
-function ProjectEditDialog(props) {
-  const { open, onClose, project, projectId, handleProjects } = props;
-  const navigate = useNavigate();
-
-  const [renameValue, setRenameValue] = useState("");
-  const handleRenameChange = (e) => setRenameValue(e.target.value);
-
-  useEffect(() => {
-    if (project) setRenameValue(project.name);
-  }, [project]);
-
-  const handleClose = () => onClose();
-
-  const handleRename = async (id, name) => {
-    await handleProjects.update(id, { name: name });
-    onClose();
-  };
-
-  const handleDelete = async (id) => {
-    await handleProjects.delete(id);
-    onClose();
-    navigate("/project/");
-  };
-
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <div
-        style={{
-          padding: "1rem",
-          display: "flex",
-          flexDirection: "column",
-          gap: "1rem",
-        }}
-      >
-        <DialogTitle>Edit project: {project.name}</DialogTitle>
-
-        <form
-          name="rename"
-          onSubmit={(event) => {
-            event.preventDefault();
-            handleRename(projectId, event.target.name.value);
-          }}
-        >
-          <TextField
-            type="text"
-            name="name"
-            label="Name"
-            value={renameValue}
-            onChange={handleRenameChange}
-          />
-          <Button type="submit" disabled={!renameValue}>
-            Rename
-          </Button>
-        </form>
-
-        <form
-          name="delete"
-          onSubmit={(event) => {
-            event.preventDefault();
-            handleDelete(projectId);
-          }}
-        >
-          <Button type="submit">Delete</Button>
-        </form>
-      </div>
-    </Dialog>
-  );
-}
+import AddIcon from "@mui/icons-material/Add";
 
 export default function ProjectsList(props) {
   const { projects, handleProjects } = useProjects();
   const navigate = useNavigate();
   const params = useParams();
 
-  const [editDialogOpen, setEditDialogOpen] = useState(false);
-  const handleEditDialogOpen = () => setEditDialogOpen(true);
-  const handleEditDialogClose = () => setEditDialogOpen(false);
-
   const [createDialogOpen, setCreateDialogOpen] = useState(false);
-  const handleCreateDialogOpen = () => setCreateDialogOpen(true);
-  const handleCreateDialogClose = () => setCreateDialogOpen(false);
+  const toggleCreateDialog = () => setCreateDialogOpen(!createDialogOpen);
 
-  const projectsList = (
+  const ProjectButtons = () => (
     <List>
       {Object.keys(projects).map((id) => (
         <ListItem
@@ -164,63 +47,59 @@ export default function ProjectsList(props) {
     </List>
   );
 
-  const projectEditButton = (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "0.5rem",
-        }}
-      >
-        <Button variant="outlined" onClick={handleEditDialogOpen}>
-          Edit
-        </Button>
-      </div>
-
-      <ProjectEditDialog
-        open={editDialogOpen}
-        onClose={handleEditDialogClose}
-        project={projects[Number(params.projectId)]}
-        projectId={Number(params.projectId)}
-        handleProjects={handleProjects}
-      />
-    </>
+  const ProjectCreateButton = () => (
+    <IconButton
+      onClick={toggleCreateDialog}
+      size={"large"}
+      sx={{ alignSelf: "center" }}
+    >
+      <AddIcon />
+    </IconButton>
   );
 
-  const projectAddButton = (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          padding: "0.5rem",
-        }}
-      >
-        <Button variant="outlined" onClick={handleCreateDialogOpen}>
-          Add new
-        </Button>
-      </div>
+  const ProjectCreateDialog = () => {
+    const handleClose = () => toggleCreateDialog();
 
-      <ProjectCreateDialog
-        open={createDialogOpen}
-        onClose={handleCreateDialogClose}
-        handleProjects={handleProjects}
-      />
-    </>
-  );
+    const [nameValue, setNameValue] = useState("");
+    const handleNameChange = (e) => setNameValue(e.target.value);
+
+    const handleCreate = async (event) => {
+      event.preventDefault();
+      await handleProjects.create(nameValue);
+      handleClose();
+    };
+
+    return (
+      <Dialog open={createDialogOpen} onClose={handleClose}>
+        <DialogTitle>Add new project</DialogTitle>
+        <form onSubmit={handleCreate}>
+          <DialogContent>
+            <TextField
+              autoFocus
+              fullWidth
+              type="text"
+              name="name"
+              placeholder="Project Name"
+              value={nameValue}
+              onChange={handleNameChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button type="submit" disabled={!nameValue}>
+              Create
+            </Button>
+            <Button onClick={handleClose}>Cancel</Button>
+          </DialogActions>
+        </form>
+      </Dialog>
+    );
+  };
 
   return (
     <>
-      {projectsList}
-      <Divider />
-      {params.projectId && projects[Number(params.projectId)] && (
-        <>
-          {projectEditButton}
-          <Divider />
-        </>
-      )}
-      {projectAddButton}
+      <ProjectButtons />
+      <ProjectCreateButton />
+      <ProjectCreateDialog />
     </>
   );
 }
