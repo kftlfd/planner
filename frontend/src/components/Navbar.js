918437592db4d96619637be1b26855a044a4b720
kftlfd <kftlfd@ex.ua>
2022-07-03 21:18:31 +0300

    navbar styling

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,24 +6,26 @@ export default function Navbar(props) {
   const auth = useAuth();
   return (
     <nav className="Navbar">
-      <div>
-        <Link to={auth.user ? "/" : "/welcome"}>Home</Link>
-      </div>
+      <div className="NavbarContent">
+        <div>
+          <Link to={auth.user ? "/" : "/welcome"}>Home</Link>
+        </div>
 
-      <div>
-        {auth.user ? (
-          <>
-            <span>{auth.user.username}</span>
-            <a href="#" onClick={auth.logout}>
-              Log Out
-            </a>
-          </>
-        ) : (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </>
-        )}
+        <div>
+          {auth.user ? (
+            <>
+              <span>{auth.user.username}</span>
+              <a href="#" onClick={auth.logout}>
+                Log Out
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
