7a8e0639b07192db0072938c120337a99daee2cf
kftlfd <kftlfd@ex.ua>
2022-07-04 14:02:58 +0300

    add theme switch, AppBar theming

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,15 @@
 import React, { useState, useMemo } from "react";
 import { Navigate, Outlet } from "react-router-dom";
+
 import { useAuth } from "../AuthContext";
 import { useProjects } from "../ProjectsContext";
 import ProjectsList from "./ProjectsList";
+import { useColorMode } from "../Theme";
 
 import {
   AppBar,
   Toolbar,
+  IconButton,
   Drawer,
   Divider,
   List,
@@ -16,11 +19,15 @@ import {
   Collapse,
   Box,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 export default function Main(props) {
   const auth = useAuth();
@@ -85,19 +92,51 @@ export default function Main(props) {
     );
   };
 
+  const ThemeSwitch = () => {
+    const colorMode = useColorMode();
+
+    return (
+      <List>
+        <ListItemButton onClick={colorMode.toggleColorMode}>
+          <ListItemIcon>
+            {colorMode.mode === "light" ? (
+              <Brightness7Icon />
+            ) : (
+              <Brightness4Icon />
+            )}
+          </ListItemIcon>
+          <ListItemText
+            primary={`Theme: ${colorMode.mode === "light" ? "Light" : "Dark"}`}
+          />
+        </ListItemButton>
+      </List>
+    );
+  };
+
   const MainContent = () => {
     return (
       <Box sx={{ flexGrow: 1, display: "flex" }}>
-        <AppBar>
-          <Toolbar />
+        <AppBar color="background">
+          <Toolbar sx={{ marginLeft: `${drawerWidth}px` }}>
+            <IconButton>
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
         </AppBar>
 
         <NavDrawer>
-          <Toolbar />
+          <Toolbar sx={{ justifyContent: "end" }}>
+            <IconButton>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
           <Divider />
           <UserButtons />
           <Divider />
           <ProjectsList />
+          <div style={{ flexGrow: 1 }} />
+          <Divider />
+          <ThemeSwitch />
         </NavDrawer>
 
         <Box sx={{ flexGrow: 1 }}>
