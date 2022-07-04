28cca893748ffaffbb9f56fcf0ccd9de05ecdd11
kftlfd <kftlfd@ex.ua>
2022-07-05 00:38:26 +0300

    restructure Main component

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,14 @@
 import React, { useState, useMemo } from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useOutletContext } from "react-router-dom";
 
 import { useAuth } from "../AuthContext";
-import { useProjects } from "../ProjectsContext";
-import ProjectsList from "./ProjectsList";
 import { useColorMode } from "../Theme";
+import { useProjects } from "../ProjectsContext";
+import LoadingApp from "./Loading";
+import ProjectsList from "./ProjectsList";
 
 import {
+  Typography,
   AppBar,
   Toolbar,
   IconButton,
@@ -28,91 +30,11 @@ import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import LoadingApp from "./Loading";
 
 export default function Main(props) {
   const auth = useAuth();
   const { loading } = useProjects();
   const drawerWidth = 240;
-
-  const NavDrawer = (props) => {
-    return (
-      <Drawer
-        variant="permanent"
-        anchor="left"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            boxSizing: "border-box",
-            width: drawerWidth,
-          },
-        }}
-      >
-        {props.children}
-      </Drawer>
-    );
-  };
-
-  const UserButtons = () => {
-    const [nestedListOpen, setNestedListOpen] = useState(false);
-    const toggleNestedList = () => setNestedListOpen(!nestedListOpen);
-
-    return (
-      <List>
-        <ListItemButton
-          key="user"
-          onClick={toggleNestedList}
-          selected={nestedListOpen}
-        >
-          <ListItemIcon>
-            <AccountCircleIcon />
-          </ListItemIcon>
-          <ListItemText>{auth.user.username}</ListItemText>
-          {nestedListOpen ? <ExpandLess /> : <ExpandMore />}
-        </ListItemButton>
-
-        <Collapse in={nestedListOpen} timeout="auto">
-          <List component="div" disablePadding>
-            <ListItemButton key="settings-button">
-              <ListItemIcon>
-                <SettingsIcon />
-              </ListItemIcon>
-              <ListItemText>Settings</ListItemText>
-            </ListItemButton>
-
-            <ListItemButton key="logout-button" onClick={auth.logout}>
-              <ListItemIcon>
-                <LogoutIcon />
-              </ListItemIcon>
-              <ListItemText>Log Out</ListItemText>
-            </ListItemButton>
-          </List>
-        </Collapse>
-      </List>
-    );
-  };
-
-  const ThemeSwitch = () => {
-    const colorMode = useColorMode();
-
-    return (
-      <List>
-        <ListItemButton onClick={colorMode.toggleColorMode}>
-          <ListItemIcon>
-            {colorMode.mode === "light" ? (
-              <Brightness7Icon />
-            ) : (
-              <Brightness4Icon />
-            )}
-          </ListItemIcon>
-          <ListItemText
-            primary={`Theme: ${colorMode.mode === "light" ? "Light" : "Dark"}`}
-          />
-        </ListItemButton>
-      </List>
-    );
-  };
 
   const MainContent = () => {
     const colorMode = useColorMode();
@@ -128,15 +50,7 @@ export default function Main(props) {
               : "background.default",
         }}
       >
-        <AppBar color="background">
-          <Toolbar sx={{ marginLeft: `${drawerWidth}px` }}>
-            <IconButton>
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-
-        <NavDrawer>
+        <MainDrawer drawerWidth={drawerWidth}>
           <Toolbar sx={{ justifyContent: "end" }}>
             <IconButton>
               <ChevronLeftIcon />
@@ -149,11 +63,10 @@ export default function Main(props) {
           <div style={{ flexGrow: 1 }} />
           <Divider />
           <ThemeSwitch />
-        </NavDrawer>
+        </MainDrawer>
 
         <Box sx={{ flexGrow: 1 }}>
-          <Toolbar />
-          <Outlet />
+          <Outlet context={{ drawerWidth }} />
         </Box>
       </Box>
     );
@@ -174,5 +87,124 @@ export default function Main(props) {
       </>
     ),
     [auth.user, loading]
+  );
+}
+
+function MainDrawer(props) {
+  return (
+    <Drawer
+      variant="permanent"
+      anchor="left"
+      sx={{
+        width: props.drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          boxSizing: "border-box",
+          width: props.drawerWidth,
+        },
+      }}
+    >
+      {props.children}
+    </Drawer>
+  );
+}
+
+export function MainHeader(props) {
+  const { drawerWidth } = useOutletContext();
+
+  return (
+    <AppBar color="background">
+      <Toolbar sx={{ marginLeft: `${drawerWidth}px` }}>
+        <IconButton sx={{ marginRight: "1rem" }}>
+          <MenuIcon />
+        </IconButton>
+
+        <Typography
+          variant="h5"
+          component="div"
+          sx={{
+            flexGrow: 1,
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+        >
+          {props.title}
+        </Typography>
+
+        {props.children}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export function MainBody(props) {
+  return (
+    <>
+      <Toolbar />
+      <div style={{ padding: "1rem" }}>{props.children}</div>
+    </>
+  );
+}
+
+function UserButtons(props) {
+  const auth = useAuth();
+
+  const [nestedListOpen, setNestedListOpen] = useState(false);
+  const toggleNestedList = () => setNestedListOpen(!nestedListOpen);
+
+  return (
+    <List>
+      <ListItemButton
+        key="user"
+        onClick={toggleNestedList}
+        selected={nestedListOpen}
+      >
+        <ListItemIcon>
+          <AccountCircleIcon />
+        </ListItemIcon>
+        <ListItemText>{auth.user.username}</ListItemText>
+        {nestedListOpen ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+
+      <Collapse in={nestedListOpen} timeout="auto">
+        <List component="div" disablePadding>
+          <ListItemButton key="settings-button">
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText>Settings</ListItemText>
+          </ListItemButton>
+
+          <ListItemButton key="logout-button" onClick={auth.logout}>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText>Log Out</ListItemText>
+          </ListItemButton>
+        </List>
+      </Collapse>
+    </List>
+  );
+}
+
+function ThemeSwitch(props) {
+  const colorMode = useColorMode();
+
+  return (
+    <List>
+      <ListItemButton onClick={colorMode.toggleColorMode}>
+        <ListItemIcon>
+          {colorMode.mode === "light" ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )}
+        </ListItemIcon>
+        <ListItemText
+          primary={`Theme: ${colorMode.mode === "light" ? "Light" : "Dark"}`}
+        />
+      </ListItemButton>
+    </List>
   );
 }
