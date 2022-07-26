faf10c1c0d6a26f5e49239ed7b043abc3878c8e4
kftlfd <kftlfd@ex.ua>
2022-07-26 22:30:08 +0300

    move to a single consumer per user

--- backend/consumers.py
+++ backend/consumers.py
@@ -3,44 +3,67 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ProjectConsumer(AsyncWebsocketConsumer):
+class UserConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        # set up connection info
+        """ establish connection """
         self.user = self.scope['user']
-        self.projectId = self.scope['url_route']['kwargs']['projectId']
-        self.project_group_name = f"project-{self.projectId}"
-
-        # join project group
-        await self.channel_layer.group_add(
-            self.project_group_name,
-            self.channel_name
-        )
-
-        # establish connection
-        await self.accept()
+        self.groups = []
+        if self.user.is_authenticated:
+            await self.accept()
 
     async def disconnect(self, close_code):
-        # leave project group
-        await self.channel_layer.group_discard(
-            self.project_group_name,
-            self.channel_name
-        )
+        """ leave all groups """
+        for group in self.groups:
+            await self.channel_layer.group_discard(
+                group,
+                self.channel_name
+            )
 
-    # receive message from WebSocket
-    # broadcast to project group
     async def receive(self, text_data):
-        await self.channel_layer.group_send(
-            self.project_group_name,
-            {
-                'type': 'message',
-                'sender': self.user,
-                'data': text_data
-            }
-        )
+        """ receive message from WebSocket"""
+        data = json.loads(text_data)
 
-    # receive message from project group
-    # send to WebSocket
+        # join group
+        if data.get('action') == 'group/join':
+            for group in data['groups']:
+                await self.channel_layer.group_add(
+                    group,
+                    self.channel_name
+                )
+            self.groups += data['groups']
+            await self.send(text_data=json.dumps({
+                'info': f"joined groups {data['groups']}",
+                'groups': self.groups
+            }))
+
+        # leave group
+        elif data.get('action') == 'group/leave':
+            for group in data['groups']:
+                await self.channel_layer.group_discard(
+                    group,
+                    self.channel_name
+                )
+            for group in data['groups']:
+                if group in self.groups:
+                    self.groups.remove(group)
+            await self.send(text_data=json.dumps({
+                'info': f"leaved groups {data['groups']}",
+                'groups': self.groups
+            }))
+
+        # broadcast to group
+        else:
+            await self.channel_layer.group_send(
+                data['group'],
+                {
+                    'type': 'message',
+                    'sender': self.user,
+                    'data': text_data
+                }
+            )
+
     async def message(self, event):
+        """receive message from project group, send to WebSocket"""
         if event['sender'] != self.user:
             await self.send(text_data=event['data'])
--- backend/routing.py
+++ backend/routing.py
@@ -4,6 +4,5 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/project/(?P<projectId>\d+)/$',
-            consumers.ProjectConsumer.as_asgi()),
+    re_path(r'ws/$', consumers.UserConsumer.as_asgi()),
 ]
