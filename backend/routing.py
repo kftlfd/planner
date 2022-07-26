57e20497306a295435c9b3b866ac017d21186ab3
kftlfd <kftlfd@ex.ua>
2022-07-26 19:39:31 +0300

    remove test route

--- backend/routing.py
+++ backend/routing.py
@@ -4,7 +4,6 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    re_path(r'ws/test/', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/project/(?P<projectId>\d+)/$',
             consumers.ProjectConsumer.as_asgi()),
 ]
