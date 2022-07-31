dc9c24b61cf3bf95b88d763e93ed0cb1846f635d
kftlfd <kftlfd@ex.ua>
2022-07-31 17:47:45 +0300

    update auth api

--- backend/auth.py
+++ backend/auth.py
@@ -1,58 +1,62 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.http import HttpResponse, JsonResponse
 from django.urls import path
 
-from .models import Account
-from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import User
+from .serializers import UserBasicSerializer
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
+
+
+@api_view(['POST'])
 def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            try:
-                user = User.objects.create_user(
-                    form.cleaned_data['username'],
-                    password=form.cleaned_data['password2']
-                )
-                user.save()
-                account = Account(user=user)
-                account.save()
-                login(request, user)
-                return JsonResponse(UserSerializer(user).data)
-            except:
-                return HttpResponse('Error occured while attempting to create new user', status=500)
-        return JsonResponse(form.errors, status=406)
-    return HttpResponse('Wrong request method.', status=400)
+    form = CustomUserCreationForm(request.data)
+    if form.is_valid():
+        try:
+            new_user = User.objects.create_user(
+                form.cleaned_data['username'],
+                password=form.cleaned_data['password2']
+            )
+            new_user.save()
+            login(request, new_user)
+            return Response(UserBasicSerializer(new_user).data)
+        except:
+            return Response('Failed to create new user', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return Response(form.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
 
 
+@api_view(['POST'])
 def login_view(request):
-    if request.method == 'POST':
-        if request.user.is_authenticated:
-            user = request.user
-        elif request.POST:
-            user = authenticate(request,
-                                username=request.POST.get('username', None),
-                                password=request.POST.get('password', None)
-                                )
-            if not user:
-                return HttpResponse('Invalid username and/or password', status=404)
-        else:
-            return HttpResponse('Not logged in', status=401)
-        login(request, user)
-        return JsonResponse(UserSerializer(user).data)
-    return HttpResponse('Wrong request method.', status=400)
+    if request.user.is_authenticated:
+        user = request.user
+    elif request.data:
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            return Response('Invalid username and/or password', status=status.HTTP_404_NOT_FOUND)
+    else:
+        return Response('Not logged in', status=status.HTTP_401_UNAUTHORIZED)
+    login(request, user)
+    return Response(UserBasicSerializer(user).data)
 
 
+@api_view(['POST'])
 def logout_view(request):
     logout(request)
-    return HttpResponse("Logged out", status=201)
+    return Response({'details': 'Logged out'})
 
 
 urlpatterns = [
-    path('register', register_view),
-    path('login', login_view),
-    path('logout', logout_view),
+    path('register/', register_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
 ]
