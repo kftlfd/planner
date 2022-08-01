b995f60c286a9a8e323f58bae08fd1b945d5e3e9
kftlfd <kftlfd@ex.ua>
2022-08-01 21:02:12 +0300

    add InputModal

--- frontend/src/layout/Modal.js
+++ frontend/src/layout/Modal.js
@@ -8,6 +8,7 @@ import {
   DialogContent,
   DialogContentText,
   DialogActions,
+  TextField,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
@@ -34,6 +35,64 @@ export function SimpleModal(props) {
       </DialogContent>
       <DialogActions>
         <Button onClick={onConfirm} color={"error"}>
+          {action}
+        </Button>
+        <Button onClick={onClose}>Cancel</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export function InputModal(props) {
+  const {
+    open,
+    onConfirm,
+    onClose,
+    title,
+    content,
+    action,
+    inputValue,
+    inputChange,
+    actionsChildren,
+  } = props;
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: "1rem",
+        }}
+      >
+        {title}
+        <IconButton onClick={onClose}>
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+
+      <DialogContent>
+        {content && <DialogContentText>{content}</DialogContentText>}
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            if (inputValue) onConfirm();
+          }}
+        >
+          <TextField
+            value={inputValue}
+            onChange={inputChange}
+            fullWidth
+            autoFocus
+            sx={{ paddingBlock: "1rem" }}
+          />
+        </form>
+      </DialogContent>
+
+      <DialogActions>
+        {actionsChildren}
+        <Button onClick={onConfirm} disabled={!inputValue}>
           {action}
         </Button>
         <Button onClick={onClose}>Cancel</Button>
