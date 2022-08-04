c862e13ef5808495ff9a76b68cf9ffb0443527b3
kftlfd <kftlfd@ex.ua>
2022-08-04 19:22:23 +0300

    connect to api; add unread badge and indicator of new messages in chatbox; load users from store; parse message time; improved styling

--- frontend/src/pages/project/ProjectChat.js
+++ frontend/src/pages/project/ProjectChat.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
+import { selectProjectChat } from "../../store/chatSlice";
+import { selectUserById, selectUserId } from "../../store/usersSlice";
+import { useActions } from "../../context/ActionsContext";
 import { Sidebar, SidebarHeader, SidebarBody } from "../../layout/Sidebar";
 
 import {
@@ -11,12 +16,18 @@ import {
   CircularProgress,
   Card,
   Avatar,
+  Fade,
+  Badge,
 } from "@mui/material";
 import ChatIcon from "@mui/icons-material/Chat";
 import SendIcon from "@mui/icons-material/Send";
 import Close from "@mui/icons-material/Close";
 
 export function ProjectChat(props) {
+  const { projectId } = useParams();
+  const projectChat = useSelector(selectProjectChat(projectId));
+  const actions = useActions();
+
   const [state, setState] = React.useState({
     chatOpen: false,
     messagesLoaded: false,
@@ -24,118 +35,106 @@ export function ProjectChat(props) {
     newMessage: "",
     loading: false,
     error: false,
+    unread: 0,
+    unreadIndex: null,
   });
   const chatToggle = () =>
     setState((prev) => ({ ...prev, chatOpen: !prev.chatOpen }));
   const newMessageChange = (e) =>
     setState((prev) => ({ ...prev, newMessage: e.target.value }));
 
-  // useSelector( projectMessges )
-  const [projectMessages, setProjectMessages] = React.useState([]);
+  //
+  // Scroll to bottom of chatbox
+  //
+  const chatboxBottom = React.useRef();
+  React.useLayoutEffect(() => {
+    chatboxBottom.current?.scrollIntoView(true);
+  }, [state.messagesLoaded]);
+  React.useLayoutEffect(() => {
+    chatboxBottom.current?.scrollIntoView({
+      block: "start",
+      behavior: "smooth",
+    });
+  }, [projectChat]);
 
-  const msgBox = React.useRef();
-  function scrollToBottom() {
-    msgBox.current?.scrollIntoView({ block: "end", behavior: "smooth" });
-  }
-  React.useEffect(scrollToBottom, [projectMessages]);
-
+  //
+  // Update unread badge
+  //
   React.useEffect(() => {
-    if (state.chatOpen && !state.messagesLoaded) {
-      // actual logic
-      //
-      // try {
-      //   actions.loadMessages
-      //   setState(messagesLoaded true)
-      // } catch (error) {
-      //   setState(messagesError true)
-      // }
-
-      //
-      // Placeholder
-      //
-      setTimeout(() => {
-        setProjectMessages(
-          [...Array(16).keys()].map((index) => ({
-            id: index,
-            text: `Message ${index}`,
-            user: "some rando",
-            time: "previously",
-          }))
-        );
-        setState((prev) => ({
-          ...prev,
-          messagesLoaded: true,
-        }));
-        setInterval(() => {
-          setProjectMessages((prev) => [
-            ...prev,
-            {
-              id: prev.length + 1,
-              text: `New message ${prev.length + 1}`,
-              user: "New user",
-              time: "Recent",
-            },
-          ]);
-        }, 10000);
-      }, 2000);
+    if (state.messagesLoaded && !state.chatOpen) {
+      setState((prev) => ({
+        ...prev,
+        unread: prev.unread + 1,
+        unreadIndex:
+          prev.unreadIndex === null ? projectChat.length - 1 : prev.unreadIndex,
+      }));
     }
-  }, [state.chatOpen, state.messagesLoaded]);
+  }, [projectChat]);
+  React.useEffect(() => {
+    if (state.unread > 0 && !state.chatOpen) {
+      setState((prev) => ({ ...prev, unread: 0, unreadIndex: null }));
+    }
+  }, [state.chatOpen]);
 
+  //
+  // Load chat messages
+  //
+  async function loadChat() {
+    try {
+      await actions.chat.load(projectId);
+      setState((prev) => ({ ...prev, messagesLoaded: true }));
+    } catch (error) {
+      console.error("Failed to load chat: ", error);
+      setState((prev) => ({ ...prev, messagesError: true }));
+    }
+  }
+  React.useEffect(() => {
+    if (!state.messagesLoaded) {
+      loadChat();
+    }
+  }, [state.messagesLoaded]);
+
+  //
+  // Send new message
+  //
   const sendDisabled = state.messagesError
     ? true
     : !state.newMessage
     ? true
     : state.loading || state.error;
-
   async function sendMessage(e) {
     e.preventDefault();
     if (sendDisabled) return;
-
-    // actual logic
-    //
-    // setState(loading true)
-    // try {
-    //   actions.sendMessage
-    //   setState(loading false)
-    // } catch (error) {
-    //   setState(loading false, error true)
-    //   setTimeout( setState(error false) )
-    // }
-
-    //
-    // Placeholder
-    //
-    setState((prev) => ({
-      ...prev,
-      loading: true,
-    }));
-
-    setTimeout(() => {
-      setProjectMessages((prev) => [
-        ...prev,
-        {
-          id: projectMessages.length + 1,
-          text: state.newMessage,
-          user: "owner",
-          time: "now",
-        },
-      ]);
-
-      setState((prev) => ({
-        ...prev,
-        loading: false,
-        newMessage: "",
-      }));
-    }, 1000);
+    setState((prev) => ({ ...prev, loading: true }));
+    try {
+      await actions.chat.newMessage(projectId, state.newMessage);
+      setState((prev) => ({ ...prev, loading: false, newMessage: "" }));
+    } catch (error) {
+      console.error("Failed to send message: ", error);
+      setState((prev) => ({ ...prev, loading: false, error: true }));
+      setTimeout(() => {
+        setState((prev) => ({ ...prev, error: false }));
+      }, 5000);
+    }
   }
 
   return (
     <>
       <IconButton onClick={chatToggle}>
-        <ChatIcon />
+        <Badge
+          badgeContent={state.unread}
+          invisible={state.unread === 0}
+          color="primary"
+        >
+          <ChatIcon />
+        </Badge>
       </IconButton>
 
-      <Sidebar open={state.chatOpen} toggle={chatToggle}>
+      <Sidebar
+        open={state.chatOpen}
+        toggle={chatToggle}
+        sx={{ overflow: "hidden" }}
+      >
         <SidebarHeader title={"Chat"} toggle={chatToggle} />
 
         <SidebarBody
@@ -144,6 +143,7 @@ export function ProjectChat(props) {
             padding: 0,
             overflow: "scroll",
             backgroundColor: "chat.bg",
+            padding: "0 1.5rem",
           }}
         >
           {state.messagesError ? (
@@ -156,24 +156,24 @@ export function ProjectChat(props) {
               Failed to load messages
             </Typography>
           ) : state.messagesLoaded ? (
-            <Box ref={msgBox} sx={{ padding: "0 1.5rem 1rem" }}>
-              {projectMessages.map((msg, index) => (
+            <>
+              {projectChat.map((msg, index) => (
                 <Message
                   key={msg.id}
                   message={msg}
                   samePrevUser={
-                    projectMessages[index - 1] &&
-                    projectMessages[index - 1].user ===
-                      projectMessages[index].user
+                    projectChat[index - 1] &&
+                    projectChat[index - 1].user === projectChat[index].user
                   }
                   sameNextUser={
-                    projectMessages[index + 1] &&
-                    projectMessages[index + 1].user ===
-                      projectMessages[index].user
+                    projectChat[index + 1] &&
+                    projectChat[index + 1].user === projectChat[index].user
                   }
+                  unread={index === state.unreadIndex}
                 />
               ))}
-            </Box>
+              <Box ref={chatboxBottom} sx={{ height: "1rem" }} />
+            </>
           ) : (
             <Box sx={{ paddingTop: "3rem", textAlign: "center" }}>
               <CircularProgress />
@@ -225,86 +225,123 @@ export function ProjectChat(props) {
 }
 
 function Message(props) {
-  const { message, samePrevUser, sameNextUser } = props;
-  const user = message.user;
-
-  // useSelector( userById(user) )
-  // useSelector( currentUser )
-
-  const self = user === "owner";
+  const { message, samePrevUser, sameNextUser, unread } = props;
+  const user = useSelector(selectUserById(message.user)) || {
+    username: "[deleted]",
+  };
+  const selfId = useSelector(selectUserId);
+  const self = message.user === selfId;
 
   function parseTime(s) {
-    // t = new Date(s)
-    // t.date, t.month, t.hour, t.minute
-    return s;
+    let d = new Date(s);
+    let formatted = d.toLocaleString(
+      {},
+      {
+        month: "short",
+        day: "numeric",
+        hour12: false,
+        hour: "numeric",
+        minute: "2-digit",
+      }
+    );
+    return formatted;
   }
 
   return (
-    <Box
-      sx={{
-        marginTop: samePrevUser ? "0.2rem" : "1rem",
-        display: "flex",
-        justifyContent: self ? "end" : "start",
-      }}
-    >
+    <Fade in={true}>
       <Box
         sx={{
+          marginTop: unread ? 0 : samePrevUser ? "0.2rem" : "1rem",
           display: "flex",
-          maxWidth: "80%",
-          gap: "0.5rem",
-          ...(self && {
-            flexDirection: "row-reverse",
-            justifyContent: "start",
-          }),
+          flexDirection: "column",
+          alignItems: self ? "flex-end" : "flex-start",
         }}
       >
-        {!self && (
-          <Box sx={{ width: "40px" }}>
-            {!samePrevUser && <Avatar>{message.user[0] || "?"}</Avatar>}
-          </Box>
+        {unread && (
+          <Typography
+            variant="body1"
+            component="div"
+            align="center"
+            sx={{
+              alignSelf: "stretch",
+              marginBlock: "1rem",
+              backgroundColor: "action.focus",
+              marginInline: "-1.5rem",
+            }}
+          >
+            New messages
+          </Typography>
         )}
-        <Card
+
+        <Box
           sx={{
-            backgroundColor: "chat.msg",
-            padding: "0.4rem 1rem",
+            maxWidth: "80%",
             display: "flex",
-            flexDirection: "column",
-            alignItems: self ? "flex-end" : "flex-start",
-            gap: "0.15rem",
-            borderRadius: "0.8rem",
-            ...(self && { borderTopRightRadius: "0.25rem" }),
-            ...(self && sameNextUser && { borderBottomRightRadius: "0.25rem" }),
-            ...(!self && { borderTopLeftRadius: "0.25rem" }),
-            ...(!self && sameNextUser && { borderBottomLeftRadius: "0.25rem" }),
+            gap: "0.5rem",
+            ...(self && {
+              flexDirection: "row-reverse",
+              justifyContent: "start",
+            }),
           }}
         >
-          <Box
+          {!self && (
+            <Box sx={{ width: "40px" }}>
+              {(!samePrevUser || unread) && (
+                <Avatar>{user.username[0] || ""}</Avatar>
+              )}
+            </Box>
+          )}
+
+          <Card
             sx={{
-              fontSize: "0.85rem",
-              fontWeight: "bold",
-              color: "primary.dark",
+              backgroundColor: "chat.msg",
+              padding: "0.4rem",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: self ? "flex-end" : "flex-start",
+              borderRadius: "0.8rem",
+              ...(self && {
+                borderTopRightRadius: "0.25rem",
+                paddingLeft: "0.8rem",
+              }),
+              ...(self &&
+                sameNextUser && { borderBottomRightRadius: "0.25rem" }),
+              ...(!self && {
+                borderTopLeftRadius: "0.25rem",
+                paddingRight: "0.8rem",
+              }),
+              ...(!self &&
+                sameNextUser && { borderBottomLeftRadius: "0.25rem" }),
             }}
           >
-            {user}
-          </Box>
-          <Box
-            sx={{
-              ...(!self && { alignSelf: "start" }),
-            }}
-          >
-            {message.text}
-          </Box>
-          <Box
-            sx={{
-              alignSelf: "end",
-              fontSize: "0.8rem",
-              color: "text.secondary",
-            }}
-          >
-            {parseTime(message.time)}
-          </Box>
-        </Card>
+            {(!samePrevUser || unread) && (
+              <Box
+                sx={{
+                  fontSize: "0.85rem",
+                  fontWeight: "bold",
+                  color: "primary.dark",
+                  marginBottom: "0.2rem",
+                }}
+              >
+                {user.username}
+              </Box>
+            )}
+
+            <Box>{message.text}</Box>
+
+            <Box
+              sx={{
+                marginTop: "0.5rem",
+                alignSelf: "flex-end",
+                fontSize: "0.8rem",
+                color: "text.secondary",
+              }}
+            >
+              {parseTime(message.time)}
+            </Box>
+          </Card>
+        </Box>
       </Box>
-    </Box>
+    </Fade>
   );
 }
