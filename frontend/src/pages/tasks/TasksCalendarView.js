a37ed2ba29d348dcb629e865f4fb490dd0bdf03f
kftlfd <kftlfd@ex.ua>
2022-08-08 14:51:10 +0300

    select date on click; show task counts in calendar; show tasks of selected day; improved styling

--- frontend/src/pages/tasks/TasksCalendarView.js
+++ frontend/src/pages/tasks/TasksCalendarView.js
@@ -7,14 +7,26 @@ import {
   addDays,
   startOfMonth,
   lastDayOfMonth,
+  isSameDay,
 } from "date-fns";
 
 import { selectAllTasks } from "../../store/tasksSlice";
+import { TaskCard } from "./TaskCard";
 
 import { Container, Card, Box, Typography, IconButton } from "@mui/material";
 import WestIcon from "@mui/icons-material/West";
+import TodayIcon from "@mui/icons-material/Today";
+
+const format = [{}, { dateStyle: "short" }];
+
+const weekend = { backgroundColor: "action.hover" };
+const weekendColor = {
+  "&:nth-of-type(7n - 1)": weekend,
+  "&:nth-of-type(7n)": weekend,
+};
 
 export function TasksCalendarView(props) {
+  const { setSelectedTask, taskDetailsToggle } = props;
   const { projectId } = useParams();
   const allTasks = useSelector(selectAllTasks);
   const tasks = Object.keys(allTasks)
@@ -44,7 +56,13 @@ export function TasksCalendarView(props) {
     setState((prev) => ({ ...prev, selectedDate: newSelect }));
   }
 
-  const format = [{}, { dateStyle: "short" }];
+  function goToToday() {
+    setState((prev) => ({ ...prev, selectedDate: prev.today }));
+  }
+
+  function selectDate(day) {
+    setState((prev) => ({ ...prev, selectedDate: day }));
+  }
 
   return (
     <>
@@ -53,29 +71,42 @@ export function TasksCalendarView(props) {
           sx={{
             marginBlock: "1rem",
             display: "flex",
-            gap: "2rem",
+            gap: "1rem",
             justifyContent: "center",
             alignItems: "center",
           }}
         >
+          <Box sx={{ flexBasis: "40px", flexShrink: 10 }} />
+
           <IconButton onClick={() => goToPrevMonth()}>
             <WestIcon />
           </IconButton>
-          <Typography variant="h6" align="center" sx={{ flexBasis: "15rem" }}>
+
+          <Typography
+            variant="h6"
+            align="center"
+            sx={{ flexBasis: "15rem", color: "text.primary" }}
+          >
             {state.selectedDate.toLocaleString(
               {},
               { month: "long", year: "numeric" }
             )}
           </Typography>
+
           <IconButton onClick={() => goToNextMonth()}>
             <WestIcon sx={{ transform: "rotate(180deg)" }} />
           </IconButton>
+
+          <IconButton onClick={goToToday}>
+            <TodayIcon />
+          </IconButton>
         </Box>
+
         <Card>
           <Box
             sx={{
               display: "grid",
-              gridTemplateColumns: "repeat(7, auto)",
+              gridTemplateColumns: "repeat(7, 1fr)",
             }}
           >
             {/* Weekdays */}
@@ -83,10 +114,11 @@ export function TasksCalendarView(props) {
               <Box
                 key={"weekday" + day}
                 sx={{
-                  paddingBlock: "0.5rem",
+                  paddingBlock: "0.8rem",
                   textAlign: "center",
-                  borderBottom: "1px solid gray",
-                  "&:not(:nth-child(7n))": { borderRight: "1px solid gray" },
+                  fontSize: "1.1rem",
+                  fontWeight: "bold",
+                  ...weekendColor,
                 }}
               >
                 {day}
@@ -104,9 +136,12 @@ export function TasksCalendarView(props) {
                   let day = addDays(lastDay, -i);
 
                   return (
-                    <div key={`${day.toISOString()}`}>
-                      {day.toLocaleString(...format)}
-                    </div>
+                    <Day
+                      key={`${day.toISOString()}`}
+                      day={day}
+                      notCurrentMonth={true}
+                      onClick={() => selectDate(day)}
+                    />
                   );
                 })
                 .reverse();
@@ -119,11 +154,21 @@ export function TasksCalendarView(props) {
 
               return [...Array(days).keys()].map((i) => {
                 let day = addDays(firstDay, i);
+                let tasks = tasksByDate[day.toLocaleString(...format)] || [];
+                let doneCount = tasks.filter(
+                  (task) => task.done === true
+                ).length;
+                let notDoneCount = tasks.length - doneCount;
 
                 return (
-                  <div key={`${day.toISOString()}`}>
-                    {day.toLocaleString(...format)}
-                  </div>
+                  <Day
+                    key={`${day.toISOString()}`}
+                    day={day}
+                    isSelected={isSameDay(day, state.selectedDate)}
+                    onClick={() => selectDate(day)}
+                    doneCount={doneCount}
+                    notDoneCount={notDoneCount}
+                  />
                 );
               });
             })()}
@@ -137,9 +182,12 @@ export function TasksCalendarView(props) {
                 let day = addDays(lastDay, 1 + i);
 
                 return (
-                  <div key={`${day.toISOString()}`}>
-                    {day.toLocaleString(...format)}
-                  </div>
+                  <Day
+                    key={`${day.toISOString()}`}
+                    day={day}
+                    notCurrentMonth={true}
+                    onClick={() => selectDate(day)}
+                  />
                 );
               });
             })()}
@@ -147,12 +195,88 @@ export function TasksCalendarView(props) {
         </Card>
       </Container>
 
-      <Container maxWidth="md">
-        <h3>Selected day</h3>
-      </Container>
+      <Container maxWidth="md" sx={{ marginTop: "1rem", marginBottom: "3rem" }}>
+        {(() => {
+          let tasks =
+            tasksByDate[state.selectedDate.toLocaleString(...format)] || [];
 
-      <pre>{JSON.stringify(state, null, 4)}</pre>
-      <pre>{JSON.stringify(tasks, null, 4)}</pre>
+          return tasks.map((task) => (
+            <TaskCard
+              key={"caltask" + task.id}
+              taskId={task.id}
+              openDetails={() => {
+                setSelectedTask(task.id);
+                taskDetailsToggle();
+              }}
+            />
+          ));
+        })()}
+      </Container>
     </>
+  );
+}
+
+const tasksCountStyle = {
+  fontSize: "0.7rem",
+  height: "100%",
+  aspectRatio: "1/1",
+  display: "grid",
+  placeContent: "center",
+  borderRadius: "50%",
+  backgroundColor: "primary.light",
+  color: "primary.contrastText",
+};
+
+function Day(props) {
+  const { day, notCurrentMonth, isSelected, onClick, doneCount, notDoneCount } =
+    props;
+
+  return (
+    <Box
+      onClick={onClick}
+      sx={{
+        cursor: "pointer",
+        ...(!isSelected && weekendColor),
+        ...(isSelected && {
+          backgroundColor: "primary.main",
+          color: "primary.contrastText",
+        }),
+      }}
+    >
+      <Box
+        sx={{
+          paddingBlock: "0.5rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "0.5rem",
+          "&:hover": { backgroundColor: "action.focus" },
+          ...(notCurrentMonth && { opacity: "0.4" }),
+        }}
+      >
+        {day.getDate()}
+        <Box sx={{ height: "1.25rem", display: "flex", gap: "0.5rem" }}>
+          {doneCount > 0 && (
+            <Box
+              sx={{
+                ...tasksCountStyle,
+                backgroundColor: "action.disabledBackground",
+              }}
+            >
+              {doneCount}
+            </Box>
+          )}
+          {notDoneCount > 0 && (
+            <Box
+              sx={{
+                ...tasksCountStyle,
+              }}
+            >
+              {notDoneCount}
+            </Box>
+          )}
+        </Box>
+      </Box>
+    </Box>
   );
 }
