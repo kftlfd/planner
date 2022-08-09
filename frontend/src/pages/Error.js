b5eecae4e481c553a3b1f70cdaab4578d5d02576
kftlfd <kftlfd@ex.ua>
2022-08-09 20:19:41 +0300

    improved Error page

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,9 +1,32 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Container, Typography, Button } from "@mui/material";
 
 export default function Error(props) {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <div>404 Not Found</div>
-    </>
+    <Container
+      maxWidth="sm"
+      sx={{
+        flexGrow: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "2rem",
+      }}
+    >
+      <Typography variant="h1" sx={{ color: "text.primary" }}>
+        404
+      </Typography>
+      <Typography variant="h4" sx={{ color: "text.primary" }}>
+        Page not found
+      </Typography>
+      <Button onClick={() => navigate("/", { replace: true })}>
+        Go to Homepage
+      </Button>
+    </Container>
   );
 }
