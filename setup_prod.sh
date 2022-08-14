22d8cb74a697e73fb38a3f860b1a2e299c03e1e5
kftlfd <kftlfd@ex.ua>
2022-08-14 18:03:02 +0300

    remove redundant commands

--- setup_prod.sh
+++ setup_prod.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 # for deployment on Heroku
-
-echo '--- preparing database ---'
 python3 manage.py makemigrations backend
 python3 manage.py migrate
-
-echo '--- collecting static files ---'
-python3 manage.py collectstatic --noinput
