0f9c667d9fe528528d3cd16933267f5d547170d9
kftlfd <kftlfd@ex.ua>
2024-09-04 23:07:23 +0300

    update eslint conf

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,14 +1,34 @@
 module.exports = {
   root: true,
+  env: { browser: true, node: true, es6: true },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     projectService: true,
     tsconfigRootDir: __dirname,
   },
-  ignorePatterns: [".eslintrc.cjs", "webpack.*.js"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/strict-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: [
+    "@typescript-eslint",
+    "react",
+    "react-hooks",
+    "jsx-a11y",
+    "simple-import-sort",
+  ],
+  rules: {
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [["^"], ["^~/", "^app/"], ["^\\."]],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+  },
 };
